81700dd797a46662fdd5bae0d48ba849c1e520ba
ytnobody <ytnobody@gmail.com>
2013-02-25 17:34:28 +0900

    色々変更。。。

--- js/rebel.js
+++ js/rebel.js
@@ -1,11 +1,30 @@
 var Rebel = {
     init: function(){
         Rebel.set_title();
+        Rebel.set_credit();
+        Rebel.adjust_content_width(window);
+        $(window).on('resize', function(e){Rebel.adjust_content_width(window)});
     },
     set_title: function(){
-        $('div.header').append('<p class="title"></p>');
-        $('p.title').text($('title').text());
-    }
+        $('header').append('<h1 class="title" />');
+        $('h1.title').text($('title').text());
+    },
+    set_credit: function(){
+        var str = $('meta[name=copyright]').attr('content') ? '&copy;' + $('meta[name=copyright]').attr('content') :
+                  $('meta[name=author]').attr('content') ? 'Author:' + $('meta[name=author]').attr('content') :
+        null ;
+        if ( str ) {
+            $('footer').append($('<address />'));
+            $('address').append(str);
+        }
+    },
+    adjust_content_width: function(b){
+        var items = $('div.main > div');
+        var w = $(b).width();
+        items.each(function(i,e){
+            $(e).width((w / items.size()) - 30);
+        });
+    },
 };
 
 Zepto(function(){
